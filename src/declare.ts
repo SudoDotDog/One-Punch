d1e90a0cb69ce30af06c83f30fedc9103f98db4b
WMXPY <wm@mengw.io>
2019-06-02 19:12:50 -0500

    declares

--- src/declare.ts
+++ src/declare.ts
@@ -6,6 +6,8 @@
 
 import * as React from "react";
 
+export type InputType = "text" | "email" | "password" | "number";
+
 export type OneHydratedProps = {
 
 };
@@ -13,13 +15,36 @@ export type OneHydratedProps = {
 export type OneInputProps = {
 
     readonly onChange: (next: string) => void;
+    readonly type: InputType;
+    readonly value: string;
+};
+
+export type OneButtonProps = {
+
+    readonly onClick: () => void;
 };
 
 export type OneComponentIndex = Partial<{
 
     input: React.ComponentType<OneInputProps>;
+    button: React.ComponentType<OneButtonProps>;
 }>;
 
-export type OneStructure = {
+export type OneStructure = Record<string, OneElement>;
 
+export type Expendables = {
+
+    readonly className?: string;
+    readonly props?: Record<string, any>;
 };
+
+export type OneElement = {
+
+    readonly role: "input";
+    readonly type: InputType;
+    readonly defaultValue: string;
+} & Expendables | {
+
+    readonly role: "button";
+    readonly onClick: () => void;
+} & Expendables;
