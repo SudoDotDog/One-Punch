34bfd888e2720f5601acf5788e4d7599364c3d52
WMXPY <wm@mengw.io>
2019-06-03 15:30:52 -0500

    major fix, add import index

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @author WMXPY
+ * @namespace One
+ * @description Index
+ */
+
+export * from "./declare";
+export { Form } from "./form";
+export * from "./frame";
+export { One } from "./one";
+export * from "./slider";
+
